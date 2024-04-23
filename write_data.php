08ed87c2490a96553b412bf0ce65c36113fb9d5c
Xi Cheng <xicheng3@outlook.com>
2024-04-23 14:57:32 -0400

    Update write_data.php

--- write_data.php
+++ write_data.php
@@ -1,7 +1,19 @@
+//<?php
+  //$post_data = json_decode(file_get_contents('php://input'), true);
+ // $data = $post_data['filedata'];
+  //$file = $post_data['filename'];
+  //$name = "data/{$file}.csv"; 
+  //file_put_contents($name, $data);
+//?>
+
 <?php
-  $post_data = json_decode(file_get_contents('php://input'), true);
-  $data = $post_data['filedata'];
-  $file = $post_data['filename'];
-  $name = "data/{$file}.csv"; 
-  file_put_contents($name, $data);
+// get the data from the POST message
+$post_data = json_decode(file_get_contents('php://input'), true);
+$data = $post_data['filedata'];
+// generate a unique ID for the file, e.g., session-6feu833950202 
+$file = uniqid("session-");
+// the directory "data" must be writable by the server
+$name = "data/{$file}.csv"; 
+// write the file to disk
+file_put_contents($name, $data);
 ?>
